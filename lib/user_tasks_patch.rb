04f7abefe435f7c50c277ff7bea0e0e4cb6f1191
Brandon Farmer <thepooreskimojoe@gmail.com>
2011-12-09 12:44:37 -0600

    Added count to top menu caption

--- lib/user_tasks_patch.rb
+++ lib/user_tasks_patch.rb
@@ -2,9 +2,16 @@ require 'user'
 
 module UserTasksPatch
   def self.included(base)
+    base.send(:include, InstanceMethods)
     base.class_eval do
       unloadable
       has_many :tasks
+    end
+  end
+  
+  module InstanceMethods
+    def tasks_top_menu
+      "Todo List (#{self.tasks.count})"
     end
   end
 end
